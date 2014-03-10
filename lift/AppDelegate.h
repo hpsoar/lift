d284760768dd7c23859af37366c68a3530a30b53
Aldrich Huang <hpsoar@gmail.com>
2014-03-10 10:18:12 +0800

    Initial Commit

---
+++ lift/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lift
+//
+//  Created by Aldrich Huang on 3/10/14.
+//  Copyright (c) 2014 beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lift/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lift
+//
+//  Created by Aldrich Huang on 3/10/14.
+//  Copyright (c) 2014 beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
